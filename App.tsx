a98a1d1126b728f489240f3b6563ae0e7138f22a
yuta.hidaka <yuta322@gmail.com>
2021-01-02 02:26:19 +0900

    add:

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Clock from './screens/Clock';
+
 import Count from './screens/Count';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Clock />
       <Count />
     </View>
   );
--- screens/Count.tsx
+++ screens/Count.tsx
@@ -1,9 +1,78 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import Clock from './Clock';
+const randomColor = require('randomcolor');
 
 export default function Count() {
+    const text = 'Prepararos para comer las uvas !!!'
+    const Cuartos = 'Los Cuartos  !!!'
+    const Campanadas = 'Las Campanadas  !!!'
+    const feliz = 'Feliz año nuevo!!!🥳'
     const [time, setTime] = useState('0000-00-00 00:00:00');
-    const [countDown, setCountDown] = useState('①②③④⑤⑥⑦⑧⑨⑩⑪⑫');
+    const [Sec, setSec] = useState(0);
+    const [hide, setHide] = useState(false);
+    const [isTest, setIsTest] = useState(false);
+    const [testText, setTestText] = useState('TEST');
+    const [bellSound, setBellSound]: any = useState();
+    const [bellSound2, setBellSound2]: any = useState();
+    const [CuartosSound, setCuartosSound]: any = useState();
+    const [cheerSound, setCheerSound]: any = useState();
+    const [displayCount, setDisplayCount]: any = useState([]);
+    const [displayText, setDisplayText] = useState(text);
+
+    const countCalc: any = (sec: number | null) => {
+        const position: number = (Number(sec) / 3) + 1;
+        const __chckColor = (v: number) => {
+            if (v <= position && position <= 12) return styles.countDownNumberDark;
+            return styles.countDownNumberWhite;
+        }
+        const count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+        const countNode = count.map((v) => {
+            return (
+                <View key={v} style={[styles.countDownNumberWrapper, __chckColor(v)]}>
+                    <Text style={[styles.countDownNumber, __chckColor(v)]}>
+                        {v}
+                    </Text>
+                </View>
+            )
+        })
+        setDisplayCount(countNode)
+    }
+    const playBellSound = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/bell.mp3')
+        );
+        setBellSound(sound);
+        await sound.playAsync();
+    }
+    const playBellSound2 = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/bell2.mp3')
+        );
+        setBellSound2(sound);
+        await sound.playAsync();
+    }
+    const playCuartos = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/cuartos.mp3')
+        );
+        setCuartosSound(sound);
+        await sound.playAsync();
+    }
+    const playCheerSound = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/cheer.mp3')
+        );
+        setCheerSound(sound);
+        await sound.playAsync();
+        // await sound.unloadAsync();
+    }
+    const sleep = (time: number) => {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
     const setClock = () => {
         const now = new Date();
         const year = now.getFullYear()
@@ -14,165 +83,105 @@ export default function Count() {
         const sec = ('0' + now.getSeconds()).slice(-2)
         const secNumber = now.getSeconds()
         const minNumber = now.getMinutes()
-        const dateNumber = now.getDate()
         const monthNumber = (now.getMonth() + 1)
-        const text = 'Prepararos para comer las uvas !!!'
-        const feliz = 'Feliz año nuevo !!!'
+        const dateNumber = now.getDate()
+        if (secNumber === Sec) return;
         setTime(`${year}-${month}-${date} ${hour}:${min}:${sec}`)
-
-        if (monthNumber !== 12 && dateNumber !== 31) return;
-
-        switch (secNumber) {
-            case 45:
-                if (minNumber !== 59) break;
-                setCountDown(text)
-                break;
-            case 46:
-                if (minNumber !== 59) break;
-                setCountDown(text)
-                break;
-            case 47:
-                if (minNumber !== 59) break;
-                setCountDown(text)
-                break;
-            case 48:
-                if (minNumber !== 59) break;
-                setCountDown(text)
-                break;
-            case 49:
-                if (minNumber !== 59) break;
-                setCountDown('❶②③④⑤⑥⑦⑧⑨⑩⑪⑫')
-                break;
-            case 50:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷③④⑤⑥⑦⑧⑨⑩⑪⑫')
-                break;
-            case 51:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸④⑤⑥⑦⑧⑨⑩⑪⑫')
-                break;
-            case 52:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹⑤⑥⑦⑧⑨⑩⑪⑫')
-                break;
-            case 53:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺⑥⑦⑧⑨⑩⑪⑫')
-                break;
-            case 54:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻⑦⑧⑨⑩⑪⑫')
-                break;
-            case 55:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻❼⑧⑨⑩⑪⑫')
-                break;
-            case 56:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻❼❽⑨⑩⑪⑫')
-                break;
-            case 57:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻❼❽❾⑩⑪⑫')
-                break;
-            case 58:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻❼❽❾❿⑪⑫')
-                break;
-            case 59:
-                if (minNumber !== 59) break;
-                setCountDown('❶❷❸❹❺❻❼❽❾❿⓫⑫')
-                break;
-            case 0:
-                if (minNumber !== 0) break;
-                setCountDown('❶❷❸❹❺❻❼❽❾❿⓫⓬')
-                break;
-            case 1:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 2:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 3:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 4:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 5:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 6:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 7:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 8:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 9:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
-            case 10:
-                if (minNumber !== 0) break;
-                setCountDown(feliz)
-                break;
+        if (isTest || monthNumber === 12 && dateNumber === 31) {
+            countCalc(secNumber);
+            setSec(secNumber)
+            if (secNumber === 0 || secNumber % 3 === 0 && secNumber <= 33) {
+                playBellSound();
+            }
+            switch (secNumber) {
+                case 0:
+                    if (cheerSound) cheerSound.unloadAsync();
+                    if (bellSound) bellSound.unloadAsync();
+                    if (bellSound2) bellSound2.unloadAsync();
+                    if (CuartosSound) CuartosSound.unloadAsync();
+                    break;
+                case 34:
+                    countCalc(-1);
+                    let tmpText = '';
+                    setHide(true);
+                    const max = 50
+                    for (let i = 0; i < max; i++) {
+                        tmpText += '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉'
+                        if (i === (max / 2)) {
+                            tmpText += feliz
+                        }
+                        setDisplayText(tmpText);
+                    }
+                    playCheerSound();
+                    break;
+                case 36:
+                    setHide(false);
+                    setDisplayText(text)
+                    if (isTest || minNumber === 59) {
+                        playBellSound2();
+                    }
+                    break;
+                case 44:
+                    playCuartos();
+                    setDisplayText(Cuartos);
+                    break;
+                case 58:
+                    if (isTest || minNumber === 59) {
+                        setDisplayText(Campanadas);
+                    } else {
+                        setDisplayText(text);
+                    }
+                    break;
+            }
         }
+
     }
-    setInterval(() => setClock(), 500);
+
+    useEffect(() => {
+        const interval = setInterval(() => setClock(), 10);
+        return () => clearInterval(interval);
+    })
+    useEffect(() => {
+        const testText = isTest ? 'TEST STOP' : 'TEST'
+        setTestText(testText)
+        if (!isTest) {
+            if (cheerSound) cheerSound.unloadAsync();
+            if (bellSound) bellSound.unloadAsync();
+            if (bellSound2) bellSound2.unloadAsync();
+            if (CuartosSound) CuartosSound.unloadAsync();
+            setDisplayText(text)
+            setHide(false)
+            countCalc(-1);
+        }
+    }, [isTest])
+
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={styles.time}>
-                    {time}
-                </Text>
-            </View>
-            <Text style={styles.countDown}>
-                {countDown}
+            <Text style={styles.displayText}>
+                {displayText}
             </Text>
-            {/* <View style={{ flexDirection: 'row' }}>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-                <Text style={styles.countDownNumber}>
-                    1
-                </Text>
-            </View> */}
+            {!hide &&
+                (<View style={styles.container}>
+                    <View style={{ marginTop: 10 }}>
+                        <Clock />
+                    </View>
+                    <View style={{ marginTop: 10 }}>
+                        <Text style={styles.time}>
+                            {time}
+                        </Text>
+                    </View>
+                    <View style={styles.displayCount}>
+                        {displayCount}
+                    </View>
+                </View>
+                )
+            }
             <View>
-                <Image source={require('../assets/tenor.gif')} />
+                <Button icon="play-circle-outline" style={{ marginTop: 20 }} color="#000000" mode="outlined" onPress={() => { setIsTest(!isTest) }}>
+                    {testText}
+                </Button>
             </View>
-        </View>
+        </View >
     )
 }
 
@@ -183,7 +192,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         fontSize: 30,
     },
+    displayCount: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
     countDown: {
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    displayText: {
         fontSize: 30,
         fontWeight: "bold"
     },
@@ -194,21 +211,29 @@ const styles = StyleSheet.create({
         width: 30,
     },
     countDownNumber: {
-        borderRadius: 50,
-        borderWidth: 1,
-        borderColor: "#000000",
         textAlign: "center",
         fontWeight: "bold",
         padding: 5,
-        width: 50,
-        height: 50,
-        fontSize: 27,
+        fontSize: 20,
+    },
+    countDownNumberWrapper: {
+        borderRadius: 50,
+        borderWidth: 0.5,
+        textAlign: "center",
+        fontWeight: "bold",
+        padding: 5,
+        width: 40,
+        height: 40,
+        fontSize: 20,
+        margin: 3
     },
     countDownNumberWhite: {
-        borderColor: "#000000"
+        backgroundColor: '#fff',
+        color: '#000000'
     },
     countDownNumberDark: {
-        borderColor: "#000000"
+        backgroundColor: '#000000',
+        color: '#fff'
     },
 
 });
