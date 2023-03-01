0be96943bd50855d7d5e90812fe865b3c4f74e1f
日高悠太 <hidakayuuta@hidakayuutas-MacBook-Air.local>
2023-03-02 01:17:00 +0900

    hoge

--- screens/Count.tsx
+++ screens/Count.tsx
@@ -1,209 +1,204 @@
-import { Audio } from 'expo-av';
-import React, { useEffect, useState } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Audio } from "expo-av";
+import React, { useEffect, useState } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 import { RFValue } from "react-native-responsive-fontsize";
-import Clock from './Clock';
-import { Logs } from 'expo'
+import Clock from "./Clock";
+import { Logs } from "expo";
+import NewClock from "./newClock";
 
-Logs.enableExpoCliLogging()
+Logs.enableExpoCliLogging();
 
-const isIos = Platform.OS === 'ios';
+const isIos = Platform.OS === "ios";
 export default function Count() {
-    const text = 'Prepararos para comer las uvas !!!'
-    const Cuartos = 'Los Cuartos  !!!'
-    const Campanadas = 'Las Campanadas  !!!'
-    const feliz = 'Feliz año nuevo!!!🥳'
-    const [time, setTime] = useState('0000-00-00 00:00:00');
-    const [Sec, setSec] = useState(0);
-    const [hide, setHide] = useState(false);
-    const [isTest, setIsTest] = useState(false);
-    const [testText, setTestText] = useState('TEST');
-    const [bellSound, setBellSound]: any = useState();
-    const [bellSound2, setBellSound2]: any = useState();
-    const [CuartosSound, setCuartosSound]: any = useState();
-    const [cheerSound, setCheerSound]: any = useState();
-    const [displayCount, setDisplayCount]: any = useState([]);
-    const [displayText, setDisplayText] = useState(text);
+  const text = "Prepararos para comer las uvas !!!";
+  const Cuartos = "Los Cuartos  !!!";
+  const Campanadas = "Las Campanadas  !!!";
+  const feliz = "Feliz año nuevo!!!🥳";
+  const [time, setTime] = useState("0000-00-00 00:00:00");
+  const [Sec, setSec] = useState(0);
+  const [hide, setHide] = useState(false);
+  const [isTest, setIsTest] = useState(false);
+  const [testText, setTestText] = useState("TEST");
+  const [bellSound, setBellSound]: any = useState();
+  const [bellSound2, setBellSound2]: any = useState();
+  const [CuartosSound, setCuartosSound]: any = useState();
+  const [cheerSound, setCheerSound]: any = useState();
+  const [displayCount, setDisplayCount]: any = useState([]);
+  const [displayText, setDisplayText] = useState(text);
 
+  const countCalc: any = (sec: number | null) => {
+    const position: number = Number(sec) / 3 + 1;
+    const __checkColor = (v: number) => {
+      if (v <= position && position <= 12) return styles.countDownNumberDark;
+      return styles.countDownNumberWhite;
+    };
+    const count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    const countCalc: any = (sec: number | null) => {
-        const position: number = (Number(sec) / 3) + 1;
-        const __checkColor = (v: number) => {
-            if (v <= position && position <= 12) return styles.countDownNumberDark;
-            return styles.countDownNumberWhite;
-        }
-        const count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    const countNode = count.map((v) => {
+      return (
+        <View key={v} style={[styles.countDownNumberWrapper, __checkColor(v)]}>
+          <Text style={[styles.countDownNumber, __checkColor(v)]}>{v}</Text>
+        </View>
+      );
+    });
+    setDisplayCount(countNode);
+  };
+  const playBellSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/bell.mp3")
+    );
+    setBellSound(sound);
+    await sound.playAsync();
+  };
+  const playBellSound2 = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/bell2.mp3")
+    );
+    setBellSound2(sound);
+    await sound.playAsync();
+  };
+  const playCuartos = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/cuartos.mp3")
+    );
+    setCuartosSound(sound);
+    await sound.playAsync();
+  };
+  const playCheerSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/cheer.mp3")
+    );
+    setCheerSound(sound);
+    await sound.playAsync();
+  };
 
-        const countNode = count.map((v) => {
-            return (
-                <View key={v} style={[styles.countDownNumberWrapper, __checkColor(v)]}>
-                    <Text style={[styles.countDownNumber, __checkColor(v)]}>
-                        {v}
-                    </Text>
-                </View>
-            )
-        })
-        setDisplayCount(countNode)
-    }
-    const playBellSound = async () => {
-        const { sound } = await Audio.Sound.createAsync(
-            require('../assets/bell.mp3')
-        );
-        setBellSound(sound);
-        await sound.playAsync();
-    }
-    const playBellSound2 = async () => {
-        const { sound } = await Audio.Sound.createAsync(
-            require('../assets/bell2.mp3')
-        );
-        setBellSound2(sound);
-        await sound.playAsync();
-    }
-    const playCuartos = async () => {
-        const { sound } = await Audio.Sound.createAsync(
-            require('../assets/cuartos.mp3')
-        );
-        setCuartosSound(sound);
-        await sound.playAsync();
-    }
-    const playCheerSound = async () => {
-        const { sound } = await Audio.Sound.createAsync(
-            require('../assets/cheer.mp3')
-        );
-        setCheerSound(sound);
-        await sound.playAsync();
-    }
+  const unloadSounds = () => {
+    if (cheerSound) cheerSound.unloadAsync();
+    if (bellSound) bellSound.unloadAsync();
+    if (CuartosSound) CuartosSound.unloadAsync();
+    if (bellSound2) bellSound2.unloadAsync();
+  };
 
-    const unloadSounds = () => {
-        if (cheerSound) cheerSound.unloadAsync();
-        if (bellSound) bellSound.unloadAsync();
-        if (CuartosSound) CuartosSound.unloadAsync();
-        if (bellSound2) bellSound2.unloadAsync();
-    }
+  const setClock = async () => {
+    const now = new Date();
+    const secNumber = now.getSeconds();
+    if (secNumber === Sec) return;
+    setSec(secNumber);
 
-    const setClock = async () => {
-        const now = new Date();
-        const secNumber = now.getSeconds()
-        if (secNumber === Sec) return;
-        setSec(secNumber)
+    const minNumber = now.getMinutes();
+    const monthNumber = now.getMonth() + 1;
+    const dateNumber = now.getDate();
+    const year = now.getFullYear();
+    const month = ("0" + monthNumber).slice(-2);
+    const date = ("0" + dateNumber).slice(-2);
+    const hour = ("0" + now.getHours()).slice(-2);
+    const min = ("0" + minNumber).slice(-2);
+    const sec = ("0" + secNumber).slice(-2);
 
-        const minNumber = now.getMinutes()
-        const monthNumber = now.getMonth() + 1
-        const dateNumber = now.getDate()
-        const year = now.getFullYear()
-        const month = ('0' + monthNumber).slice(-2)
-        const date = ('0' + dateNumber).slice(-2)
-        const hour = ('0' + now.getHours()).slice(-2)
-        const min = ('0' + minNumber).slice(-2)
-        const sec = ('0' + secNumber).slice(-2)
+    setTime(`${year}-${month}-${date} ${hour}:${min}:${sec}`);
 
-        setTime(`${year}-${month}-${date} ${hour}:${min}:${sec}`)
+    if (
+      isTest ||
+      (monthNumber === 12 && dateNumber === 31) ||
+      (monthNumber === 1 && dateNumber === 1)
+    ) {
+      console.log(secNumber);
+      if (
+        (secNumber === 0 || secNumber % 3 === 0) &&
+        secNumber <= 33 &&
+        monthNumber === 1
+      ) {
+        countCalc(secNumber);
+        playBellSound();
+      }
 
-        if (isTest || monthNumber === 12 && dateNumber === 31 || monthNumber === 1 && dateNumber === 1) {
-            console.log(secNumber)
-            if ((secNumber === 0 || secNumber % 3 === 0) && secNumber <= 33 && monthNumber === 1) {
-                countCalc(secNumber);
-                playBellSound();
+      switch (secNumber) {
+        case 0:
+          unloadSounds();
+          break;
+        case 34:
+          if (!isTest && monthNumber === 12) break;
+          countCalc(-1);
+          let tmpText = "";
+          setHide(true);
+          const max = 50;
+          for (let i = 0; i < max; i++) {
+            tmpText += "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉";
+            if (i === max / 2) {
+              tmpText += feliz;
             }
-
-            switch (secNumber) {
-                case 0:
-                    unloadSounds()
-                    break;
-                case 34:
-                    if (!isTest && monthNumber === 12) break;
-                    countCalc(-1);
-                    let tmpText = '';
-                    setHide(true);
-                    const max = 50
-                    for (let i = 0; i < max; i++) {
-                        tmpText += '🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉'
-                        if (i === (max / 2)) {
-                            tmpText += feliz
-                        }
-                        setDisplayText(tmpText);
-                    }
-                    playCheerSound();
-                    break;
-                case 36:
-                    if (!isTest && monthNumber === 12) break;
-                    setHide(false);
-                    setDisplayText(text)
-                    if (isTest || minNumber === 59) {
-                        playBellSound2();
-                    }
-                    break;
-                case 44:
-                    if (!isTest && monthNumber === 1) {
-                        break;
-                    };
-                    playCuartos()
-                    setDisplayText(Cuartos);
-                    break;
-                case 58:
-                    if (isTest || minNumber === 59 && monthNumber === 1) {
-                        setDisplayText(Campanadas);
-                    } else {
-                        setDisplayText(text);
-                    }
-                    break;
-            }
-        }
-
+            setDisplayText(tmpText);
+          }
+          playCheerSound();
+          break;
+        case 36:
+          if (!isTest && monthNumber === 12) break;
+          setHide(false);
+          setDisplayText(text);
+          if (isTest || minNumber === 59) {
+            playBellSound2();
+          }
+          break;
+        case 44:
+          if (!isTest && monthNumber === 1) {
+            break;
+          }
+          playCuartos();
+          setDisplayText(Cuartos);
+          break;
+        case 58:
+          if (isTest || (minNumber === 59 && monthNumber === 1)) {
+            setDisplayText(Campanadas);
+          } else {
+            setDisplayText(text);
+          }
+          break;
+      }
     }
+  };
 
-    useEffect(() => {
-        return CuartosSound
-            ? () => {
-                console.log('Unloading Sound');
-                CuartosSound.unloadAsync();
-            }
-            : undefined;
-    }, [CuartosSound]);
-
-    useEffect(() => {
-        const interval = setInterval(() => setClock(), 10);
-        return () => clearInterval(interval);
-    })
-    useEffect(() => {
-        const testText = isTest ? 'TEST STOP' : 'TEST'
-        setTestText(testText)
-        if (!isTest) {
-            if (cheerSound) cheerSound.unloadAsync();
-            if (bellSound) bellSound.unloadAsync();
-            if (bellSound2) bellSound2.unloadAsync();
-            if (CuartosSound) CuartosSound.unloadAsync();
-            setDisplayText(text)
-            setHide(false)
-            countCalc(-1);
+  useEffect(() => {
+    return CuartosSound
+      ? () => {
+          console.log("Unloading Sound");
+          CuartosSound.unloadAsync();
         }
-    }, [isTest])
+      : undefined;
+  }, [CuartosSound]);
 
-    return (
+  useEffect(() => {
+    const interval = setInterval(() => setClock(), 10);
+    return () => clearInterval(interval);
+  });
+  useEffect(() => {
+    const testText = isTest ? "TEST STOP" : "TEST";
+    setTestText(testText);
+    if (!isTest) {
+      if (cheerSound) cheerSound.unloadAsync();
+      if (bellSound) bellSound.unloadAsync();
+      if (bellSound2) bellSound2.unloadAsync();
+      if (CuartosSound) CuartosSound.unloadAsync();
+      setDisplayText(text);
+      setHide(false);
+      countCalc(-1);
+    }
+  }, [isTest]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.displayTextContainer}>
+        <Text style={styles.displayText}>{displayText}</Text>
+      </View>
+      {!hide && (
         <View style={styles.container}>
-
-            <Text style={styles.displayText}>
-                {displayText}
-            </Text>
-            {!hide &&
-                (<View style={styles.container}>
-                    <View style={{ marginTop: 5 }}>
-                        <Clock />
-                    </View>
-                    <View style={{ marginTop: 10 }}>
-                        <Text style={styles.time}>
-                            {time}
-                        </Text>
-                    </View>
-                    <View style={styles.displayCount}>
-                        {displayCount}
-                    </View>
-                </View>
-                )
-            }
-            <View>
-                <Button
+          <View style={{ marginTop: 5 }}>
+            <NewClock />
+          </View>
+        </View>
+      )}
+      <View>
+        {/* <Button
                     icon="play-circle-outline"
                     style={{ marginTop: 20, marginBottom: 50 }}
                     color="#000000"
@@ -211,62 +206,52 @@ export default function Count() {
                     onPress={() => { setIsTest(!isTest) }}
                 >
                     {testText}
-                </Button>
-            </View>
-        </View >
-    )
+                </Button> */}
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: RFValue(15),
-    },
-    displayCount: {
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-    },
-    countDown: {
-        fontSize: RFValue(15),
-        fontWeight: "bold"
-    },
-    displayText: {
-        fontSize: RFValue(35),
-        fontWeight: "bold"
-    },
-    time: {
-        fontSize: RFValue(25),
-    },
-    img: {
-        width: 30,
-    },
-    countDownNumber: {
-        textAlign: "center",
-        fontWeight: "bold",
-        padding: 5,
-        fontSize: RFValue(14),
-    },
-    countDownNumberWrapper: {
-        borderRadius: 50,
-        borderWidth: 0.5,
-        textAlign: "center",
-        fontWeight: "bold",
-        padding: 5,
-        width: isIos ? 35 : 50,
-        height: isIos ? 35 : 50,
-        fontSize: RFValue(20),
-        margin: 3
-    },
-    countDownNumberWhite: {
-        backgroundColor: '#fff',
-        color: '#000000'
-    },
-    countDownNumberDark: {
-        backgroundColor: '#000000',
-        color: '#fff'
-    },
-
-
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    fontSize: RFValue(15),
+  },
+  displayText: {
+    fontSize: RFValue(20),
+    fontWeight: "bold",
+    marginTop: "15%",
+    color: "grey",
+  },
+  displayTextContainer: {
+    position: "absolute",
+    top: 0,
+  },
+  countDownNumber: {
+    textAlign: "center",
+    fontWeight: "bold",
+    padding: 5,
+    fontSize: RFValue(14),
+  },
+  countDownNumberWrapper: {
+    borderRadius: 50,
+    borderWidth: 0.5,
+    textAlign: "center",
+    fontWeight: "bold",
+    padding: 5,
+    width: isIos ? 35 : 50,
+    height: isIos ? 35 : 50,
+    fontSize: RFValue(20),
+    margin: 3,
+  },
+  countDownNumberWhite: {
+    backgroundColor: "#fff",
+    color: "#000000",
+  },
+  countDownNumberDark: {
+    backgroundColor: "#000000",
+    color: "#fff",
+  },
 });
---
+++ screens/newClock.tsx
@@ -0,0 +1,259 @@
+import { Audio } from "expo-av";
+import React, { useEffect, useState } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Logs } from "expo";
+import { black } from "react-native-paper/lib/typescript/styles/colors";
+
+type Props = {
+  size?: number;
+  onChange?: (v: Date) => void;
+};
+
+const HOURS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export default function Clock(props: Props) {
+  const [hourDegree, setHourDegree] = useState(0);
+  const [minDegree, setMinDegree] = useState(0);
+  const [secDegree, setSecDegree] = useState(0);
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval2 = setInterval(() => {
+      const now = new Date();
+      setTime(now);
+      setHourDegree(now.getHours() * 30);
+      setMinDegree(now.getMinutes() * 6);
+      setSecDegree((now.getSeconds() + now.getMilliseconds() / 1000) * 6);
+    }, 100);
+    return () => clearInterval(interval2);
+  }, []);
+
+  useEffect(() => props?.onChange && props.onChange(time), [time]);
+
+  return (
+    <>
+      <View style={styles.container}>
+        {/* Time */}
+        <View style={[styles.datetimeContainer]}>
+          <View style={[styles.dateContainer]}>
+            <Text style={[styles.dateText]}>
+              {`0${time.getMonth() + 1}`.slice(-2)}
+            </Text>
+            {/* <Text style={[styles.dateText]}></Text> */}
+            <Text style={[styles.dateText]}>
+              {`0${time.getDate()}`.slice(-2)}
+            </Text>
+          </View>
+          <View style={[styles.timeContainer]}>
+            <Text style={[styles.timeText]}>
+              {`0${time.getHours()}`.slice(-2)}
+            </Text>
+            <Text style={[styles.timeText]}>
+              {`0${time.getMinutes()}`.slice(-2)}
+            </Text>
+            <Text style={[styles.timeText]}>
+              {`0${time.getSeconds()}`.slice(-2)}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.clockContainer}>
+          {HOURS.map((v) => {
+            return (
+              <View
+                style={[
+                  styles.objContainer,
+                  { transform: [{ rotate: `${v * 30}deg` }] },
+                ]}
+              >
+                <View
+                  style={[
+                    styles.obj,
+                    Math.abs(time.getHours() - 12) === v && {
+                      backgroundColor: "#aacf53",
+                    },
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.objText,
+                      Math.abs(time.getHours() - 12) === v && {
+                        color: "white",
+                      },
+                    ]}
+                  >
+                    {v}
+                  </Text>
+                </View>
+              </View>
+            );
+          })}
+          {HOURS.map((v) => {
+            return (
+              <View
+                style={[
+                  styles.objContainerMin,
+                  { transform: [{ rotate: `${v * 30}deg` }] },
+                ]}
+              >
+                <View style={[styles.objMin]}>
+                  <Text style={[styles.objTextMin]}>{v * 5}</Text>
+                </View>
+              </View>
+            );
+          })}
+          <View
+            style={[
+              styles.center,
+              { transform: [{ rotate: `${minDegree}deg` }] },
+            ]}
+          >
+            <View style={styles.min}></View>
+          </View>
+          <View
+            style={[
+              styles.center,
+              { transform: [{ rotate: `${secDegree}deg` }] },
+            ]}
+          >
+            <View style={styles.sec}></View>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: "auto",
+    textAlign: "center",
+  },
+  datetimeContainer: {
+    width: 150,
+    height: "100%",
+    position: "absolute",
+    flex: 1,
+    alignSelf: "center",
+    paddingVertical: "50%",
+  },
+  timeContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  dateContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  dateText: {
+    fontSize: RFValue(40),
+    flex: 1,
+    alignSelf: "center",
+    textAlign: "center",
+    color: "grey",
+  },
+  timeText: {
+    fontSize: RFValue(35),
+    flex: 1,
+    textAlign: "center",
+    color: "grey",
+  },
+  clockContainer: {
+    width: 20,
+    height: 300,
+    margin: "auto",
+    textAlign: "center",
+    display: "flex",
+  },
+  center: {
+    width: 20,
+    height: "100%",
+    position: "absolute",
+    textAlign: "center",
+    margin: "auto",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  min: {
+    height: 15,
+    width: 10,
+    backgroundColor: "#aacf53",
+    borderRadius: 9999,
+    top: "15%",
+  },
+  sec: {
+    height: 15,
+    width: 10,
+    // backgroundColor: "grey",
+    backgroundColor: "#aacf53",
+    borderRadius: 9999,
+    top: "15%",
+  },
+  obj: {
+    color: "grey",
+    position: "absolute",
+    borderRadius: 999999,
+    flex: 1,
+    alignContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    height: 25,
+    width: 25,
+  },
+  objContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+  },
+  objText: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "grey",
+    flex: 1,
+    alignContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginVertical: "auto",
+  },
+  objMin: {
+    color: "grey",
+    position: "absolute",
+    borderRadius: 999999,
+    flex: 1,
+    alignContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    height: 25,
+    width: 25,
+    marginTop: 20,
+  },
+  objContainerMin: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    alignContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    margin: 0,
+  },
+  objTextMin: {
+    fontSize: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "grey",
+    flex: 1,
+    alignContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginVertical: "auto",
+  },
+});
